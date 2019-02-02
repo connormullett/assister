a0818080e2398c229d4af728f3aac855ef4792f2
connor <connormullett@gmail.com>
2019-02-01 20:45:16 -0500

    if i used pandas dataframes, i dont have to use ids ...

--- assister/todo_handler.py
+++ assister/todo_handler.py
@@ -2,11 +2,13 @@
 import sys
 import os
 import csv
+import pandas as pd
 
 TODO_FILE = os.path.dirname(__file__) + '/todo.csv'
 
 
 def get_rows():
+    # TODO: refactor this for pandas
     f = open(TODO_FILE)
     reader = csv.reader(f, delimiter=',')
     rows = []
@@ -23,24 +25,44 @@ def get_id():
     for row in rows:
         _.append(row[0])
     del _[0]
+    if len(rows) == 1:
+        return 1
     todos = []
     for i in _:
         a = int(i)
         todos.append(a)
 
-    # TODO: Deleting rows prevents this from working properly
-    try:
-        return next(a for a, b in enumerate(todos, (len(todos) + 1)) if a != b)
-    except Exception:
-        return 1
+    return next(a for a, b in enumerate(todos, (len(todos) + 1)) if a != b)
 
 
 def todo_create():
 
-    # while True:
-    title = input('Enter title( max 20 )\n>>> ')
-    content = input('Enter Content ( max 50 )\n>>> ')
-    due = input('Enter due date ( yy/mm/dd )\n>>> ')
+    # TODO: horribly hard coded, gonna create a month wrapper
+    # for the monstrosity on 53
+    while True:
+        title = input('Enter title( max 20 )\n>>> ')
+        if len(title) > 20:
+            continue
+        else:
+            break
+    while True:
+        content = input('Enter Content ( max 50 )\n>>> ')
+        if len(content) > 50:
+            continue
+        else:
+            break
+    while True:
+        due = input('Enter due date ( yy/mm/dd )\n>>> ')
+        _ = due.split('/')
+        i = [int(a) for a in _]
+        for d in i:
+            if len(str(abs(d))) != 2:
+                continue
+        if i[1] > 12:
+            continue
+        if i[2] > 31:
+            continue
+        break
     todo_id = get_id()
 
     f = open(TODO_FILE, 'a')
@@ -50,10 +72,11 @@ def todo_create():
 
 
 def view_todos():
-    f = open(TODO_FILE, 'r')
-    for row in f.readlines():
-        print(row.strip('\n'))
-    f.close()
+    f = pd.read_csv(TODO_FILE, 'r')
+    # for row in f.readlines():
+    #     print(row.strip('\n'))
+    df = pd.DataFrame(f)
+    print(df)
 
 
 def todo_router(t):
