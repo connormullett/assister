8d940d6fdbfbcbc73e4c04d42f7d805a74d099d9
connormullett <connormullett@gmail.com>
2019-02-05 00:11:38 -0500

    more refactoring to classes

--- assister/todo_handler.py
+++ assister/todo_handler.py
@@ -9,79 +9,103 @@ TODO_FILE = os.path.dirname(__file__) + '/todo.csv'
 
 # MAKE THIS A CLASS, __del__ maybe and __add__
 
-def todo_create():
 
-    while True:
-        title = input('Enter title( max 20 )\n>>> ')
-        if len(title) > 20:
-            continue
-        else:
-            break
-    while True:
-        content = input('Enter Content ( max 50 )\n>>> ')
-        if len(content) > 50:
-            continue
-        else:
-            break
-    while True:
-        due = input('Enter due date ( yy/mm/dd )\n>>> ')
-        _ = due.split('/')
-        i = [int(a) for a in _]
-        for d in i:
-            if len(str(abs(d))) != 2:
+class WriteOut:
+    '''use stdout to write to the console'''
+    def __init__():
+        pass
+
+    def write_error(self, err):
+        sys.stdout.write(err)
+        sys.exit(1)
+
+    def write_success(self, msg):
+        sys.stdout.write(msg)
+        sys.exit(0)
+
+
+class ReadOut:
+
+    def reader(self, msg)
+        i = input(msg)
+        return i
+
+
+class TodoService:
+    def todo_create():
+
+        while True:
+            title = input('Enter title( max 20 )\n>>> ')
+            if len(title) > 20:
                 continue
-        if i[1] > 12:
+            else:
+                break
+        while True:
+            content = input('Enter Content ( max 50 )\n>>> ')
+            if len(content) > 50:
+                continue
+            else:
+                break
+        while True:
+            due = input('Enter due date ( yy/mm/dd )\n>>> ')
+            _ = due.split('/')
+            i = [int(a) for a in _]
+            for d in i:
+                if len(str(abs(d))) != 2:
+                    continue
+            if i[1] > 12:
             continue
         if i[2] > 31:
             continue
         break
 
-    f = open(TODO_FILE, 'a')
-    writer = csv.writer(f, delimiter=',')
-    writer.writerow([title, content, 'false', due])
-    sys.stdout.write('todo created successfully\n')
-    f.close()
+        f = open(TODO_FILE, 'a')
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow([title, content, 'false', due])
+        sys.stdout.write('todo created successfully\n')
+        f.close()
 
 
-def read_todos():
-    f = pd.read_csv(TODO_FILE, 'r', delimiter=',')
-    df = pd.DataFrame(f)
-    return df
+    def read_todos():
+        f = pd.read_csv(TODO_FILE, 'r', delimiter=',')
+        df = pd.DataFrame(f)
+        return df
 
 
-def view_todos():
-    df = read_todos()
-    if not df.empty:
-        print(df.to_string())
-    else:
-        print('No Todos')
+    def view_todos():
+        df = read_todos()
+        if not df.empty:
+            print(df.to_string())
+        else:
+            print('No Todos')
 
 
-def todo_delete(t):
+    def todo_delete(t):
 
-    df = read_todos()
-    try:
-        i = int(t)
-    except Exception:
-        print('{} is not a valid row identifier'.format(t))
-        sys.exit(0)
+        df = read_todos()
+        try:
+            i = int(t)
+        except Exception:
+            print('{} is not a valid row identifier'.format(t))
+            sys.exit(0)
 
-    try:
-        print(df.iloc[i].to_string())
-        choice = input('are you sure you want to delete this todo? (y/n)\n')
-    except Exception:
-        sys.stdout.write('Invalid ID\n')
-        sys.exit(0)
-    if choice.lower() == 'n':
-        sys.exit(0)
-    elif choice.lower() == 'y':
-        with open(TODO_FILE, 'r') as f:
-            rows = f.readlines()
-            del rows[i + 1]
+        try:
+            print(df.iloc[i].to_string())
+            choice = input('are you sure you want to delete this todo? (y/n)\n')
+        except Exception:
+            sys.stdout.write('Invalid ID\n')
+            sys.exit(0)
+        if choice.lower() == 'n':
+            sys.exit(0)
+        elif choice.lower() == 'y':
+            with open(TODO_FILE, 'r') as f:
+                rows = f.readlines()
+                del rows[i + 1]
 
-        with open(TODO_FILE, 'w') as f:
-            for row in rows:
-                f.write(row)
+            with open(TODO_FILE, 'w') as f:
+                for row in rows:
+                    f.write(row)
+            sys.stdout.write('Todo Deleted Successfully\n')
 
 
 class TodoRouter():
@@ -95,6 +119,8 @@ class TodoRouter():
         else:
             self.router(self.t[0])
 
+    # Refactor to function map indexing
+    # if a in arg_router_map ... where map is a dict
     def router(self, a):
         if a == 'create':
             todo_create()
@@ -107,11 +133,9 @@ class TodoRouter():
             print('Unknown argument\n' +
                   'Use assister -h for commands')
 
-    # args for -t
-    # pass t[1] to these functions
     def todo_arg_router(self):
         if self.t[0] == 'del':
-            todo_delete(t[1])
+            todo_delete(selft[1])
         elif self.t[0] == 'mi':
             pass
         elif self.t[0] == 'mc':
