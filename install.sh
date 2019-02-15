777c344c9b7d82ddb82b7c759203c51d60cdf48d
connormullett <connormullett@gmail.com>
2019-02-15 11:51:58 -0500

    recloning

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 
-pip3 install -e .
+pip3 install -e --user .
 pip3 install -r requirements.txt
 touch assister/todo.csv
-chmod 644 assister/todo.csv
+chmod 0755 assister/todo.csv
 echo 'title, content, complete, due' > assister/todo.csv
 
