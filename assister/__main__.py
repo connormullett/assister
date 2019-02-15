ff6e0e17592add33ebefc5186e285421902472d5
connormullett <connormullett@gmail.com>
2019-02-14 19:21:24 -0500

    added versioning with assister version

--- assister/__main__.py
+++ assister/__main__.py
@@ -5,18 +5,23 @@ import sys
 from .api_requester.api_router import ApiRouter
 
 from .todos.todo_router import TodoRouter
-# TODO: Add versioning
 
+with open('config.txt') as cfg:
+    VERSION = cfg.read()
 
 parser = argparse.ArgumentParser(description='Productivity without a mouse')
 parser.add_argument('command', default=None, action='store', nargs='+')
-# parser.add_argument('-a', '--api', help='perform api requests', default=None, action='store', dest='a', nargs='+')
 args = parser.parse_args()
 
 
 def main():
 
     command = args.command[0]
+
+    if command == 'version':
+        sys.stdout.write(VERSION)
+        exit(0)
+
     if command == 'todo':
         r = TodoRouter(args.command[1:])
     elif command == 'api':
