22c7d5ddd158132ab5217502ca8b42b344895438
connormullett <connormullett@gmail.com>
2019-02-15 22:44:36 -0500

    1.0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
         name='cm-assister',
         author='Connor Mullett',
-        version='1.0.0',
+        version='1.0.2',
         description='Simple Assister CLI, https://github.com/connormullett/assister',
-        packages=['assister', find_packages(exclude='tests')],
+        packages=['assister/todos', 'assister/api_requester', 'assister/dir_builder'],
         include_package_data=True,
         license='MIT',
         url='https://github.com/connormullett/assister',
